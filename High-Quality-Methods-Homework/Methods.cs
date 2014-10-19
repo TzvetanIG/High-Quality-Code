38a3436f81630d8d3990f20d20236ddd5110d5df
TzvetanIG <cecoig@mail.bg>
2014-10-19 20:35:20 +0300

    Add Homeworks/High-Quality-Methods-Homework

---
+++ High-Quality-Methods-Homework/Methods.cs
@@ -0,0 +1,145 @@
+﻿using System;
+
+namespace Methods
+{
+    class Methods
+    {
+        static double CalcTriangleArea(double a, double b, double c)
+        {
+            if (a <= 0 || b <= 0 || c <= 0)
+            {
+                throw new ArithmeticException("Sides should be positive.");
+                //return -1;
+            }
+
+            double s = (a + b + c) / 2;
+            double area = Math.Sqrt(s * (s - a) * (s - b) * (s - c));
+            return area;
+        }
+
+        static string NumberToDigit(int number)
+        {
+            switch (number)
+            {
+                case 0: return "zero";
+                case 1: return "one";
+                case 2: return "two";
+                case 3: return "three";
+                case 4: return "four";
+                case 5: return "five";
+                case 6: return "six";
+                case 7: return "seven";
+                case 8: return "eight";
+                case 9: return "nine";
+                default: throw new ArgumentOutOfRangeException("number", "Number should be in range [0..9].");
+            }
+
+            // return "Invalid number!";
+        }
+
+        static int FindMax(params int[] elements)
+        {
+            if (elements == null || elements.Length == 0)
+            {
+                throw new ArgumentNullException("elements");
+                // return -1;
+            }
+
+            //for (int i = 1; i < elements.Length; i++)
+            //{
+            //    if (elements[i] > elements[0])
+            //    {
+            //        elements[0] = elements[i];
+            //    }
+            //}
+
+            //return elements[0];
+
+            var maxElement = elements[0];
+            for (int i = 0; i < elements.Length; i++)
+            {
+                if (elements[i] > maxElement)
+                {
+                    maxElement = elements[i];
+                }
+            }
+
+            return maxElement;
+        }
+
+        static void PrintAsNumber(object number, string format)
+        {
+            if (format == "f")
+            {
+                Console.WriteLine("{0:f2}", number);
+            }
+            else if (format == "%")
+            {
+                Console.WriteLine("{0:p0}", number);
+            }
+            else if (format == "r")
+            {
+                Console.WriteLine("{0,8}", number);
+            }
+            else
+            {
+                throw new ArgumentException("Wrong number format", "format");
+            }
+        }
+
+
+        static double CalcDistance(double x1, double y1, double x2, double y2,
+            out bool isHorizontal, out bool isVertical)
+        {
+            isHorizontal = false;
+            isVertical = false;
+            if ((y1 == y2) && (x1 == x2))
+            {
+                return 0;
+            }
+
+            if (y1 == y2)
+            {
+                isHorizontal = true;
+            }
+
+            if (x1 == x2)
+            {
+                isVertical = true;
+            }
+
+            // isHorizontal = (y1 == y2);
+            // isVertical = (x1 == x2);
+
+            var distance = Math.Sqrt((x2 - x1) * (x2 - x1) + (y2 - y1) * (y2 - y1));
+            return distance;
+        }
+
+        static void Main()
+        {
+            Console.WriteLine(CalcTriangleArea(3, 4, 5));
+
+            Console.WriteLine(NumberToDigit(5));
+
+            Console.WriteLine(FindMax(1, -5, 2));
+
+            PrintAsNumber(1.3, "f");
+            PrintAsNumber(0.75, "%");
+            PrintAsNumber(2.30, "r");
+
+            bool horizontal, vertical;
+            Console.WriteLine(CalcDistance(0, -1, 0, -1, out horizontal, out vertical));
+            Console.WriteLine("Horizontal? " + horizontal);
+            Console.WriteLine("Vertical? " + vertical);
+
+            var peter = new Student() { FirstName = "Peter", LastName = "Ivanov" };
+            peter.OtherInfo = "From Sofia, born at 17.03.1992";
+
+            Student stella = new Student() { FirstName = "Stella", LastName = "Markova" };
+            stella.OtherInfo = "From Vidin, gamer, high results, born at 03.11.1993";
+
+            Console.WriteLine("{0} older than {1} -> {2}",
+                peter.FirstName, stella.FirstName, peter.IsOlderThan(stella));
+        }
+    }
+}
---
+++ High-Quality-Methods-Homework/Student.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace Methods
+{
+    class Student
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string OtherInfo { get; set; }
+
+        public bool IsOlderThan(Student other)
+        {
+            DateTime firstDate =
+                DateTime.Parse(this.OtherInfo.Substring(this.OtherInfo.Length - 10));
+            DateTime secondDate =
+                DateTime.Parse(other.OtherInfo.Substring(other.OtherInfo.Length - 10));
+            var firstAge = DateTime.Now - firstDate;
+            var secondAge = DateTime.Now - secondDate;
+            return firstAge > secondAge;
+        }
+    }
+}
